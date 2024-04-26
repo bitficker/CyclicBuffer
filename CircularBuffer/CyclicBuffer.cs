d922900d5e685c0d2409195a8b6bc32ea1b126da
bitficker <shopper.tiago@gmail.com>
2024-04-26 18:47:08 -0300

    identifying full buffer

--- CircularBuffer/CyclicBuffer.cs
+++ CircularBuffer/CyclicBuffer.cs
@@ -1,13 +1,15 @@
-﻿namespace CircularBuffer;
+﻿using System.Text;
 
-public class CyclicBufferContainer<T>
+namespace CircularBuffer;
+
+public class CyclicBufferContainer<T> 
 {
     private int Size { get; set; }
     private T[] Buffer { get; set; }
 
-    private int Head = 0;
+    private int Head = 0; // Write
 
-    private int Tail = 0;
+    private int Tail = 0; // Read
 
     public CyclicBufferContainer(int size)
     {
@@ -17,26 +19,43 @@ public class CyclicBufferContainer<T>
         this.Buffer = new T[Size];
     }
 
-    public void Enqueue(T value)
+    public int Enqueue(T value)
     {
-        var idx = Head % Size;
-        
-        Buffer[idx] = value;
-        Head++;
-    }
+        if (IsFull()) return 0;
 
+        if (Head == Size - 1)
+        {
+            Head = 0;
+        }
+        else
+        {
+            Head++;
+        }
+        
+        Buffer[Head] = value;
+        Head++;
+
+        return 1;
+    }
     public T? Dequeue()
     {
         if (IsEmpty())
             return default(T); // what is the default of T in some possible cases
         
-        var idx = Tail % Size;
-        
         Tail++;
-        return Buffer[idx];
-
+        return Buffer[Tail];
     }
-    
     private bool IsEmpty() => Head == Tail;
+    private bool IsFull() => Head - 1 == Tail || (Head == Size - 1 && Tail == 0);
+    public override string ToString()
+    {
+        var builder = new StringBuilder(Size);
+
+        foreach (var elem in Buffer)
+            builder.Append(elem);
+
+        
+        return builder.ToString();
+    }
     
 }
